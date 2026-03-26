f34ed7be32d39ec605a76a69270360e8a3d46615
David <pro@Davids-iMac.local>
2026-03-26 19:14:49 -0400

    fix

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,17 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "www.zerotowealthpro.com" }],
+        destination: "https://zerotowealthpro.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
